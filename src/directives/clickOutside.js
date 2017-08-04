fabb3e789d786dd521c3803e8d3a2ee59740e957
islam3zzat <islam3zzat@gmail.com>
2017-08-04 14:29:13 +0200

    handle click outside

---
+++ src/directives/clickOutside.js
@@ -0,0 +1,15 @@
+const clickOutside = {
+  bind (el, binding) {
+    el.addEventListener('click', clickOutside.stopProp)
+    document.body.addEventListener('click', binding.value)
+
+  },
+  
+  unbind(el, binding) {
+    el.removeEventListener('click', clickOutside.stopProp)
+    document.body.removeEventListener('click', binding.value)
+  },
+  
+  stopProp(event) { event.stopPropagation() }
+}
+ export default clickOutside
