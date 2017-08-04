744a2f89c2e5caa59f578e7c999b898009eaef11
islam3zzat <islam3zzat@gmail.com>
2017-08-04 11:03:16 +0200

    initialize component calling

--- demo/components/Hello.vue
+++ demo/components/Hello.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="hello">
-    <Autocomplete
-      :path="path"
-      v-model="msg">
-      <template slot="options" scope="{options}">
-        <div>
-          <p v-for="option in options">
-            {{option}}
-          </p>
-        </div>
-      </template>
-    </Autocomplete>
-    {{msg}}
+    <div class="row">
+      <div class="col-md-6">
+        repos
+      </div>
+      <div class="col-md-6">
+        <Autocomplete
+          :path="path"
+          v-model="msg">
+          <template slot="options" scope="{options}">
+            <!--<div>-->
+            <!--<p v-for="option in options">-->
+            <!--{{option}}-->
+            <!--</p>-->
+            <!--</div>-->
+          </template>
+        </Autocomplete>
+      </div>
+    </div>
   </div>
 </template>
 
