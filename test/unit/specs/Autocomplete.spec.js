6f45286f6d1cc7ecbd2f67210acb01d6887436d0
islam3zzat <islam3zzat@gmail.com>
2017-08-10 22:48:14 +0200

    add test for initially empty input

--- test/unit/specs/Autocomplete.spec.js
+++ test/unit/specs/Autocomplete.spec.js
@@ -8,10 +8,16 @@ describe('Autocomplete.vue', () => {
     expect(vm.$el.querySelector('input').getAttribute('placeholder'))
       .to.equal('Placeholder')
   })
-  it('should have no options initialy', () => {
+  it('should have no options initially', () => {
     const Constructor = Vue.extend(Autocomplete)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('ul').childElementCount)
       .to.equal(0)
+  })
+  it('should be initially empty', () => {
+    const Constructor = Vue.extend(Autocomplete)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('input').value)
+      .to.equal('')
   })
 })
