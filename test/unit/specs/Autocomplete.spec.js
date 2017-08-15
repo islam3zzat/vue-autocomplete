62b47fa14de42307a12df20dc512f4e066720f27
islam3zzat <islam3zzat@gmail.com>
2017-08-15 22:02:03 +0200

    add test for before search

--- test/unit/specs/Autocomplete.spec.js
+++ test/unit/specs/Autocomplete.spec.js
@@ -24,20 +24,42 @@ describe('Autocomplete.vue', () => {
     expect(vm.$el.querySelector('ul').childElementCount)
       .to.equal(0)
   })
-  it('should be initially empty', () => {
+  it('should be initially empty', (done) => {
     const Constructor = Vue.extend(Autocomplete)
-    const vm = new Constructor().$mount()
+    const vm = new Constructor({
+      propsData: {
+      }
+    }).$mount()
     expect(vm.$el.querySelector('input').value)
       .to.equal('')
+    done()
   })
   it('should execute transform function', (done) => {
+    done()
   })
   it('should execute afterSearch function', (done) => {
+    done()
   })
   it('should execute beforeSearch function', (done) => {
+    const Constructor = Vue.extend(Autocomplete)
+    const beforeSearch = sinon.spy()
+    const vm = new Constructor({
+      propsData: {
+        beforeSearch,
+        source: []
+      }
+    }).$mount()
+    vm.$refs.val.value = '1231'
+    vm.$refs.val.dispatchEvent(new Event('input'))
+    setTimeout(() => {
+      expect(beforeSearch).to.have.been.calledOnce
+      done()
+    }, 400)
   })
   it('should execute beforeUpdateValue function', (done) => {
+    done()
   })
   it('should execute afterUpdateValue function', (done) => {
+    done()
   })
 })
